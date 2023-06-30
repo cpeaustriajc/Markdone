c677e8e9d0d5e33859daae2bd346e2e6b426895b
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-06-30 22:32:50 +0800

    Use `react-markdown` package

--- components/preview.tsx
+++ components/preview.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { createElement, Fragment } from "react";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
-import remarkRehype from "remark-rehype/lib";
-import rehypeReact from "rehype-react/lib";
+import ReactMarkdown from "react-markdown";
 
 type Props = {
   doc: string;
@@ -13,20 +9,12 @@ type Props = {
 export const Preview = (props: Props) => {
   const { doc } = props;
 
-  const md = unified()
-    .use(remarkParse)
-    .use(remarkGfm)
-    .use(remarkRehype)
-    .use(rehypeReact, {
-      createElement,
-      Fragment,
-    })
-    .processSync(doc).result;
-
   return (
-    <div className="h-full basis-1/2 grow-0 shrink-0 overflow-x-hidden overflow-y-auto pl-3 border-l border-gray-400 prose max-w-full dark:prose-invert">
-      {md}
-    </div>
+    <ReactMarkdown
+      children={doc}
+      remarkPlugins={[[remarkGfm, { tableCellPadding: true }]]}
+      className="h-full basis-1/2 grow-0 shrink-0 overflow-x-hidden overflow-y-auto pl-3 border-l border-gray-400 prose max-w-full dark:prose-invert"
+    />
   );
 };
 
