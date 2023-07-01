59d769439031c9722b2dac91794de65dd8105b85
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-01 10:42:38 +0800

    Change spinner color

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -3,7 +3,7 @@ const Spinner: React.FC = () => {
     <div role="status">
       <svg
         aria-hidden="true"
-        className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="w-8 h-8 mr-2 text-zinc-300 animate-spin dark:text-zinc-600 fill-sky-500"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
