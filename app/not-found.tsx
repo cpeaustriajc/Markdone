95c90d496b024c9b178c7709407e93206c177e90
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-04 23:47:18 +0800

    Add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        404 - Not Found
+      </h2>
+      <p className="text-sm">
+        Go back to{' '}
+        <Button asChild variant="link" className="p-0">
+          <Link href="/">home</Link>
+        </Button>
+      </p>
+    </main>
+  )
+}
