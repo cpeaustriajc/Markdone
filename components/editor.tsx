0542c55ef8d318511d8d0229d9fff3f13d545428
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-05 14:55:18 +0800

    Rework line numbers, add tab support

--- components/editor.tsx
+++ components/editor.tsx
@@ -2,33 +2,55 @@
 
 import { markdownAtom } from '@/app/store'
 import { useAtom } from 'jotai'
-import { useRef } from 'react'
+import {
+  memo,
+  useMemo,
+  useRef,
+} from 'react'
 
 export function Editor() {
   const [doc, setDoc] = useAtom(markdownAtom)
   const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+  const lineNumber = useMemo(() => doc.split('\n').length, [doc])
+  const longestString = useMemo(
+    () =>
+      doc.split('\n').reduce((a, b) => (a.length > b.length ? a : b)).length,
+    [doc]
+  )
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     e.preventDefault()
     setDoc(e.target.value)
   }
 
-  const computeLineCount = (): number => {
-    if (textAreaRef.current) {
-      const { value, rows } = textAreaRef.current
-      if (value) {
-        return Math.max(value.split('\n').length, rows)
-      }
-      return rows
-    }
-    return 1
+  const insertTab = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const textArea = textAreaRef.current!
+    const start = textArea.selectionStart
+    const end = textArea.selectionEnd
+
+    textArea.value =
+      textAreaRef.current?.value.substring(0, start) +
+      '\t' +
+      textAreaRef.current?.value.substring(end)
+
+    e.preventDefault()
   }
 
-  const LineNumbers = () => {
-    return Array.from({ length: computeLineCount() }).map((_, index) => (
-      <span key={index}>{index + 1}</span>
-    ))
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    switch (e.key) {
+      case 'Tab':
+        insertTab(e)
+    }
   }
+
+  const LineNumbers = memo(() => {
+    return Array.from({ length: lineNumber }).map((_, index) => (
+      <span className="select-none" key={index}>
+        {index + 1}
+      </span>
+    ))
+  })
 
   return (
     <div className="shrink-0 grow-0 basis-1/2  font-mono text-sm">
@@ -43,11 +65,15 @@ export function Editor() {
           ref={textAreaRef}
           id="markdown-editor"
           name="markdown-editor"
+          cols={longestString}
+          rows={lineNumber}
           className="w-full resize-none overflow-x-auto break-keep bg-background pl-1 outline-none focus:border-none active:border-none"
           onChange={handleChange}
+          onKeyDown={handleKeyDown}
           wrap="off"
           value={doc}
-        />
+        >
+        </textarea>
       </div>
     </div>
   )
