1d8b187ae6068699716944b454347484c49c0eba
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-05 12:28:37 +0800

    remove max-w-full

--- components/preview.tsx
+++ components/preview.tsx
@@ -12,7 +12,7 @@ export function Preview() {
     <ReactMarkdown
       children={doc}
       remarkPlugins={[[remarkGfm, { tableCellPadding: true }]]}
-      className="prose h-full max-w-full shrink-0 grow-0 basis-1/2 overflow-y-auto overflow-x-hidden pl-3 dark:prose-invert"
+      className="prose h-full shrink-0 grow-0 basis-1/2 overflow-y-auto overflow-x-hidden pl-3 dark:prose-invert"
     />
   )
 }
