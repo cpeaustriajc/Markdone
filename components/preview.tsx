5e785327f15d4dcbcf0bf02997d3464474279e2f
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-05 21:34:43 +0800

    Fix formatting

--- components/preview.tsx
+++ components/preview.tsx
@@ -17,9 +17,9 @@ export function Preview({
       ref={previewRef}
       className="prose h-full shrink-0 grow-0 basis-1/2 overflow-y-auto overflow-x-hidden pl-3 dark:prose-invert"
     >
-      <ReactMarkdown
-        remarkPlugins={[[remarkGfm, { tableCellPadding: true }]]}
-      >{doc}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[[remarkGfm, { tableCellPadding: true }]]}>
+        {doc}
+      </ReactMarkdown>
     </div>
   )
 }
