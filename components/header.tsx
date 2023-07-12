862957d0175e403ec51c55fd9a2d5ba6f141cd25
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-12 22:03:18 +0800

    Extract menu component

--- components/header.tsx
+++ components/header.tsx
@@ -1,68 +1,17 @@
 'use client'
 
-import { filenameAtom, markdownAtom } from '@/app/store'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
-import { HamburgerMenuIcon } from '@radix-ui/react-icons'
-import { useAtom } from 'jotai'
-import { Button } from './ui/button'
+import { filenameAtom } from '@/app/store'
+import { useAtomValue } from 'jotai'
+import { Menu } from './menu'
 
 export function Header() {
-	const [doc, setDoc] = useAtom(markdownAtom)
-	const [filename, setFilename] = useAtom(filenameAtom)
+	const filename = useAtomValue(filenameAtom)
 
-	const openFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const reader = new FileReader()
 
-		if (e.target.files === null) throw new Error('No file selected')
-
-		reader.readAsText(e.target.files[0])
-
-		reader.onload = () => {
-			let text = reader.result
-			setDoc(text as string)
-			if (e.target.files === null) throw new Error('No file selected')
-			localStorage.removeItem('markdown')
-			localStorage.setItem(
-				'markdown',
-				JSON.stringify({
-					filename: e.target.files[0].name,
-					content: reader.result,
-				}),
-			)
-		}
-	}
-
-	const saveFile = () => {
-		const element = document.createElement('a')
-		const file = new Blob([doc], { type: 'text/markdown; charset=UTF-8; variant=GFM' })
-		element.href = URL.createObjectURL(file)
-		element.download = filename
-		document.body.appendChild(element)
-		element.click()
-	}
 
 	return (
 		<header className="flex h-16 items-center gap-1.5 border-b bg-background px-4 text-foreground">
-			<Sheet>
-				<SheetTrigger>
-					<span className="sr-only">Open Menu</span> <HamburgerMenuIcon className="h-6 w-6" />
-				</SheetTrigger>
-				<SheetContent side="left">
-					<SheetHeader>
-						<SheetTitle>Menu</SheetTitle>
-					</SheetHeader>
-					<div className="grid w-full gap-1.5">
-						<Label htmlFor="upload">Open File</Label>
-						<Input id="upload" type="file" onChange={openFile} />
-						<Label htmlFor="save">Save File</Label>
-						<Input id="save" value={filename} onChange={e => setFilename(e.target.value)} required />
-						<Button onClick={saveFile}>Save File</Button>
-					</div>
-				</SheetContent>
-			</Sheet>
-
+			<Menu/>
 			<h2 className="scroll-m-20 text-xl font-semibold tracking-tight">
 				{filename.substring(0, filename.lastIndexOf('.'))}
 			</h2>
---
+++ components/menu.tsx
@@ -0,0 +1,61 @@
+import { filenameAtom } from '@/app/store'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import { HamburgerMenuIcon } from '@radix-ui/react-icons'
+import { useAtom } from 'jotai'
+import { Button } from './ui/button'
+
+export function Menu() {
+	const [filename, setFilename] = useAtom(filenameAtom)
+	const [doc, setDoc] = useAtom(filenameAtom)
+
+	const openFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const reader = new FileReader()
+
+		if (e.target.files === null) throw new Error('No file selected')
+
+		reader.readAsText(e.target.files[0])
+
+		reader.onload = () => {
+			let text = reader.result
+			setDoc(text as string)
+			if (e.target.files === null) throw new Error('No file selected')
+			localStorage.removeItem('markdown')
+			localStorage.setItem(
+				'markdown',
+				JSON.stringify({
+					filename: e.target.files[0].name,
+					content: reader.result,
+				}),
+			)
+		}
+	}
+
+	const saveFile = () => {
+		const element = document.createElement('a')
+		const file = new Blob([doc], { type: 'text/markdown; charset=UTF-8; variant=GFM' })
+		element.href = URL.createObjectURL(file)
+		element.download = filename
+		document.body.appendChild(element)
+		element.click()
+	}
+
+return (	<Sheet>
+	<SheetTrigger>
+		<span className="sr-only">Open Menu</span> <HamburgerMenuIcon className="h-6 w-6" />
+	</SheetTrigger>
+	<SheetContent side="left">
+		<SheetHeader>
+			<SheetTitle>Menu</SheetTitle>
+		</SheetHeader>
+		<div className="grid w-full gap-1.5">
+			<Label htmlFor="upload">Open File</Label>
+			<Input id="upload" type="file" onChange={openFile} />
+			<Label htmlFor="save">Save File</Label>
+			<Input id="save" value={filename} onChange={e => setFilename(e.target.value)} required />
+			<Button onClick={saveFile}>Save File</Button>
+		</div>
+	</SheetContent>
+</Sheet>)
+}
