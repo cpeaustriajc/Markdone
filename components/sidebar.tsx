4afce0805a1a5e84dcd9b1d295e71211d21b9bef
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-23 12:59:45 +0800

    Prevent layout shift

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -16,29 +16,31 @@ export function Sidebar() {
 
 	return (
 		<Sheet>
-			{domLoaded && (
-				<SheetTrigger asChild>
-					<Button variant="ghost">
-						<span className="sr-only">Open Menu</span> <HamburgerMenuIcon className="h-6 w-6" />
-					</Button>
-				</SheetTrigger>
-			)}
+			<div className="h-9 w-14">
+				{domLoaded && (
+					<SheetTrigger asChild>
+						<Button variant="ghost">
+							<span className="sr-only">Open Menu</span> <HamburgerMenuIcon className="h-6 w-6" />
+						</Button>
+					</SheetTrigger>
+				)}
+			</div>
 			<SheetContent side="left">
 				<SheetHeader>
 					<SheetTitle>Menu</SheetTitle>
 				</SheetHeader>
 				<div className="grid w-full gap-1.5">
-						<Button
-							className="justify-start text-left"
-							onClick={() => {
-								dispatch({ type: 'CREATE_DRAFT' })
-							}}>
-							New
-						</Button>
-						<h2 className="text-lg font-semibold text-foreground">Drafts</h2>
-						{/* <Button className="justify-start text-left" variant="ghost" asChild> */}
-							{/* <Link href={"/" + state.draft.id}>{state.draft.filename}</Link> */}
-						{/* </Button> */}
+					<Button
+						className="justify-start text-left"
+						onClick={() => {
+							dispatch({ type: 'CREATE_DRAFT' })
+						}}>
+						New
+					</Button>
+					<h2 className="text-lg font-semibold text-foreground">Drafts</h2>
+					{/* <Button className="justify-start text-left" variant="ghost" asChild> */}
+					{/* <Link href={"/" + state.draft.id}>{state.draft.filename}</Link> */}
+					{/* </Button> */}
 				</div>
 			</SheetContent>
 		</Sheet>
