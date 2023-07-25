c7ead66f4dae551790fc365a53bfa1c7228eb168
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-25 21:06:38 +0800

    Remove EditorContextProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,11 @@
 'use client'
 
-import { EditorContextProvider } from '@/context/editor-context'
 import { ThemeProvider } from 'next-themes'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<ThemeProvider attribute="class" enableSystem defaultTheme="system">
-			<EditorContextProvider>{children}</EditorContextProvider>
+			{children}
 		</ThemeProvider>
 	)
 }
