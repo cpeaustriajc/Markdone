8d7243417a4c93678093adf9fce508fd3addaf6d
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-26 20:36:11 +0800

    Remove unused getSavedDrafts

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,21 +6,6 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
 
-export function getSavedDrafts(): MarkdownData | undefined {
-	try {
-		if (typeof window === 'undefined') return
-
-		const markdown = localStorage.getItem('markdown')
-
-		if (markdown === null) return undefined
-
-		return JSON.parse(markdown) as MarkdownData
-	} catch (error) {
-		console.log('Error while parsing saved drafts: ', error)
-		return undefined
-	}
-}
-
 export function toKebabCase(input: string) {
 	return input
 		.toLowerCase()
