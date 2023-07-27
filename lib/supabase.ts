aabc20fa4eac0809a365877a62f3e3add79dbc95
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-27 18:23:15 +0800

    fallback to @supabase/supabase-js

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,10 @@
 import { Database } from '@/types/database.types'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@supabase/supabase-js'
 
-export const supabaseClient = createClientComponentClient<Database>()
+export const supabaseClient = createClient<Database>(
+	process.env.NEXT_PUBLIC_SUPABASE_URL,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+)
 
 export const getDrafts = async () => {
 	try {
