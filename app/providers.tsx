a2d8377f676fc5c3be852eda649aa141e8769092
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-27 18:20:16 +0800

    Remove session provider in favor of supabase's

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-import { SessionProvider } from '@/providers/session'
+import { supabaseClient } from '@/lib/supabase'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { ThemeProvider } from 'next-themes'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<ThemeProvider attribute="class" enableSystem defaultTheme="system">
-			<SessionProvider>{children}</SessionProvider>
+			<SessionContextProvider supabaseClient={supabaseClient}>
+				{children}
+			</SessionContextProvider>
 		</ThemeProvider>
 	)
 }
