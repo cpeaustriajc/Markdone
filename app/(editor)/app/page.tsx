c1ddb5a49ab35e3cb38e49ebdf26f5ef70fd912f
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-28 18:30:28 +0800

    Add app page

---
+++ app/(editor)/app/page.tsx
@@ -0,0 +1,9 @@
+import LegacyEditor from '@/components/legacy-editor'
+
+export default function DraftsPage() {
+	return (
+		<main className="container flex h-[calc(100%-4rem)] flex-col md:flex-row">
+			<LegacyEditor />
+		</main>
+	)
+}
