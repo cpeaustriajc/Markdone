e78f878a8b29ee81c6b08bfe3eccd11ec78c7431
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-28 14:49:08 +0800

    Move non standard file conventions out the app dir

--- app/(editor)/layout.tsx
+++ app/(editor)/layout.tsx
@@ -1,22 +1,20 @@
+import { Header } from '@/components/header'
 import { inter, roboto_mono } from '@/lib/fonts'
 import Providers from '@/lib/providers'
 import '@/styles/globals.css'
-
-export const metadata = {
-	title: 'Markdone | Get more things done with Markdone!',
-	description: 'Get more things done with Markdone!',
-	manifest: '/site.webmanifest',
-}
 
 interface Props {
 	children: React.ReactNode
 }
 
-export default function RootLayout(props: Props) {
+export default function EditorRootLayout(props: Props) {
 	return (
 		<html lang="en" className={`${inter.variable} ${roboto_mono.variable} h-full`} suppressHydrationWarning>
 			<body className="h-full bg-background text-foreground">
-				<Providers>{props.children}</Providers>
+				<Providers>
+					<Header />
+					{props.children}
+				</Providers>
 			</body>
 		</html>
 	)
