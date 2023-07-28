0f0f2e9e46e0133531e127f2d4bc7f5fd04ef361
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-28 22:04:58 +0800

    Make mode-toggle proportional to other nav items

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -9,7 +9,7 @@ export function ModeToggle() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="ghost" size="icon">
+				<Button variant="ghost">
 					<span className="sr-only">Toggle theme</span>
 					<SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 					<MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
