d346860b13910a58577754b0074359fd3331f12c
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-07-30 21:39:53 +0800

    Change next/link->react-router-dom

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { Link }from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Pencil2Icon } from "@radix-ui/react-icons";
 
@@ -6,7 +6,7 @@ export function NavigationHome() {
 	return (
 		<div>
 			<Button asChild variant="ghost">
-				<Link href="/">
+				<Link to="/">
 					<span className="sr-only">Home</span>
 					<Pencil2Icon className="h-6 w-6" />
 				</Link>
