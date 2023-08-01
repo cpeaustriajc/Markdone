a53fc0f03b869d1e04b7122ad2c67fb3a5e29e45
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-08-01 21:55:14 +0800

    Initial landing page setup

---
+++ apps/www/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+	return <p>Landing Page for markdone</p>
+}
---
+++ apps/www/tailwind.config.js
@@ -0,0 +1,6 @@
+const sharedConfig = require('tailwind-config/tailwind.config.cjs')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	presets: [sharedConfig]
+}
