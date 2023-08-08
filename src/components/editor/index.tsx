a10db5da7e50f333cc54b815e7c727d22b86d4d4
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-08-08 11:06:04 +0800

    Inital lexical editor setup

---
+++ src/components/editor/index.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { inter } from '@/lib/fonts'
+import { InitialConfigType, LexicalComposer } from '@lexical/react/LexicalComposer'
+import { ContentEditable } from '@lexical/react/LexicalContentEditable'
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin'
+import { EditorThemeClasses } from 'lexical'
+import './default.css'
+
+const theme: EditorThemeClasses = {}
+
+function onError(error: Error) {
+	console.error(error)
+}
+
+export function EditorV2() {
+	const initialConfig: InitialConfigType = {
+		namespace: 'Editor',
+		theme,
+		onError,
+	}
+
+	const Placeholder: React.FC = () => {
+		return (
+			<div className={`absolute left-10 top-2 text-gray-50 opacity-30 ${inter.className}`}>
+				Enter some text...
+			</div>
+		)
+	}
+
+	return (
+		<LexicalComposer initialConfig={initialConfig}>
+			<HistoryPlugin />
+			<PlainTextPlugin
+				contentEditable={
+					<ContentEditable
+						className={`h-full w-full self-center justify-self-center rounded border border-border p-2 ${inter.className}`}
+						title="Editor"
+					/>
+				}
+				placeholder={<Placeholder />}
+				ErrorBoundary={LexicalErrorBoundary}
+			/>
+		</LexicalComposer>
+	)
+}
