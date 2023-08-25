77f4d5ef5a42b7ffb9162ed2d0b343f96dfe1d82
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-08-25 14:10:07 +0800

    Use cx provided by cva

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,8 @@
-import { ClassValue, clsx } from 'clsx'
+import { CxOptions, cx } from 'class-variance-authority'
 import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs))
+export function cn(...inputs: CxOptions) {
+	return twMerge(cx(inputs))
 }
 
 export function toKebabCase(input: string) {
