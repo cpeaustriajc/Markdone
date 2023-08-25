0f80ef414435c126ad2f91cd87478a7c25d6bed0
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-08-25 12:45:17 +0800

    Use metadata files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,10 @@ import { Providers } from '@/lib/providers'
 import { inter, roboto_mono } from '@/lib/fonts'
 
 export const metadata = {
-	icons: [
-		{ rel: 'icon', type: 'image/x-icon', url: '/favicon.ico' },
-		{ rel: 'icon', type: 'image/png', sizes: '16x16', url: '/icon-16.png' },
-		{ rel: 'icon', type: 'image/png', sizes: '32x32', url: '/icon-32.png' },
-	],
 	themeColor: [
 		{ color: 'black', media: '(prefers-color-scheme: dark)' },
 		{ color: 'white', media: '(prefers-color-scheme: light)' },
 	],
-	manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
