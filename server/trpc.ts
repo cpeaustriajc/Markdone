1ed64feb4a03114a3eab8f48127480f674e6da61
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2023-08-29 11:37:34 +0800

    Remove the clerk context

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,26 +1,12 @@
-import { TRPCError, initTRPC } from '@trpc/server'
+import { initTRPC } from '@trpc/server'
 import SuperJson from 'superjson'
-import { Context } from './context'
 
-const t = initTRPC.context<Context>().create({
+const t = initTRPC.create({
 	transformer: SuperJson,
 	errorFormatter: ({ shape }) => {
 		return shape
 	},
 })
 
-const isAuthed = t.middleware(({ next, ctx }) => {
-	if (!ctx.auth.userId) {
-		throw new TRPCError({ code: 'UNAUTHORIZED' })
-	}
-
-	return next({
-		ctx: {
-			auth: ctx.auth,
-		},
-	})
-})
-
 export const router = t.router
 export const publicProcedure = t.procedure
-export const protectedProcedure = t.procedure.use(isAuthed)
