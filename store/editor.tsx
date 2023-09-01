a27158473cad8b7f613e850196352599acb6201c
Jayzers <cpe.austria.johncarlo@gmail.com>
2023-09-01 06:16:43 +0000

    Persist store on sessionStorage

--- store/editor.tsx
+++ store/editor.tsx
@@ -1,11 +1,19 @@
-import { create } from 'zustand'
+import { StateCreator, create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 interface EditorState {
 	content: string
 	setContent: (content: string) => void
 }
 
-export const useDraftsStore = create<EditorState>(set => ({
+const draftsStore: StateCreator<EditorState> = set => ({
 	content: '',
 	setContent: content => set({ content }),
-}))
+})
+
+export const useDraftsStore = create(
+	persist<EditorState>(draftsStore, {
+		name: 'markdone:drafts',
+		storage: createJSONStorage(() => sessionStorage),
+	}),
+)
