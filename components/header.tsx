d25e6f46bd61506785ba0c7dc86f2c2c7f5ad5c0
Jayzers <cpe.austria.johncarlo@gmail.com>
2023-09-01 05:53:19 +0000

    Delete drawer component

--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,17 @@
 'use client'
 
 import { NavigationSocials } from '@/components/navigation-socials'
-import { Drawer } from '@/components/drawer'
 import { NavigationMenuItem, NavigationMenuList, NavigationMenu } from '@/components/ui/navigation-menu'
-import { CheckCircledIcon } from '@radix-ui/react-icons'
+import { HomeIcon } from '@radix-ui/react-icons'
 import { Input } from './ui/input'
 import { ModeToggle } from './mode-toggle'
 import { trpc } from '@/lib/trpc/client'
-import { Suspense, useCallback, useState } from 'react'
-import { Spinner } from './ui/spinner'
-import { Skeleton } from './ui/skeleton'
+import { useCallback, useState } from 'react'
 import { serverClient } from '@/lib/trpc/serverClient'
 import { UserButton } from '@clerk/nextjs'
 import { debounce } from 'lodash'
+import Link from 'next/link'
+import { Button } from './ui/button'
 
 type Props = {
 	id: string
@@ -20,6 +19,7 @@ type Props = {
 }
 
 export function Header({ id, initialDraft }: Props) {
+	const [filename, setFilename] = useState<string | undefined>('')
 	const [draft, draftQuery] = trpc.draft.byId.useSuspenseQuery(
 		{ id },
 		{
@@ -29,42 +29,39 @@ export function Header({ id, initialDraft }: Props) {
 			},
 		},
 	)
-	const [filename, setFilename] = useState<string | undefined>('')
-	const { mutate } = trpc.draft.updateFilename.useMutation({
+	const { mutate, isLoading } = trpc.draft.updateFilename.useMutation({
 		onSettled: () => {
 			draftQuery.refetch()
 		},
 	})
-	const mutation = debounce((filename) => {
+	const mutation = debounce(filename => {
 		mutate({ id, filename })
 	}, 1500)
 
-
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	const save = useCallback((filename: string) => mutation(filename), [])
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		save(e.target.value)
+	const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+		save(e.currentTarget.value)
 	}
-	const TitleSkeleton = () => <Skeleton className="h-9 w-32 bg-secondary"></Skeleton>
 
 	return (
 		<header className="container flex h-16 items-center justify-between gap-1.5 bg-background text-foreground">
 			<div className="flex flex-row items-center gap-2">
-				<Drawer />
+				<Link href="/">
+					<HomeIcon className="h-8 w-8" />
+				</Link>
 				<div className="flex gap-2">
 					<div className="flex min-w-[128px] flex-row gap-2">
-						<Suspense fallback={<TitleSkeleton />}>
-							<Input
-								type="text"
-								className="max-w-fit rounded-md border-none bg-background p-2 text-2xl font-bold"
-								value={filename}
-								disabled={!draft}
-								onChange={handleChange}
-							/>
-						</Suspense>
-						<Suspense fallback={<Spinner />}>
-							<CheckCircledIcon className="h-9 w-9" />
-						</Suspense>
+						<Input
+							type="text"
+							className="max-w-fit rounded-md border-none bg-background p-2 text-2xl font-bold"
+							value={draft?.filename}
+							disabled={isLoading}
+							onChange={() => setFilename(filename)}
+						/>
+						<Button type="submit" onClick={handleSubmit} disabled={isLoading}>
+							Submit
+						</Button>
 					</div>
 				</div>
 			</div>
