dc23b653aad49b95ed850bb85dde909decfc38bf
Jayzers <cpe.austria.johncarlo@gmail.com>
2023-09-04 00:18:12 +0000

    Let global-error.tsx have its own html and body tag
    ref: https://nextjs.org/docs/app/api-reference/file-conventions/error#global-errorjs

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -4,9 +4,13 @@ type Props = { error: Error; reset: () => void }
 
 export default function Error({ reset }: Props) {
 	return (
-		<div>
-			<h2>Something went wrong!</h2>
-			<button onClick={() => reset()}>Try again</button>
-		</div>
+		<html>
+			<body>
+				<div>
+					<h2>Something went wrong!</h2>
+					<button onClick={() => reset()}>Try again</button>
+				</div>
+			</body>
+		</html>
 	)
 }
