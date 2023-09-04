6ee4e6a2ba65b5bad1a08f9e3ede798e92cf8193
Jayzers <cpe.austria.johncarlo@gmail.com>
2023-09-04 00:10:07 +0000

    Don't use form element

--- app/(drafts)/@empty/page.tsx
+++ app/(drafts)/@empty/page.tsx
@@ -1,15 +1,17 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { createDraft } from '@/app/_actions/draft'
+import { useTransition } from 'react'
 
 export default function Page() {
+	const [isPending, startTransition] = useTransition()
 	return (
 		<>
 			<h1 className="w-52 scroll-m-20 text-center font-semibold">You have no files open</h1>
-			<form action={createDraft}>
-				<Button type="submit" className="w-52">
+				<Button type="submit" className="w-52" onClick={() => startTransition(() => createDraft())} disabled={isPending}>
 					New
 				</Button>
-			</form>
 		</>
 	)
 }
