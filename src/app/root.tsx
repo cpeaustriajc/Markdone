88f55efa759f7590f1fcde229423cbb59ea492c3
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-03-01 13:40:17 +0800

    Add FileInfo interface

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { listen } from '@tauri-apps/api/event'
 import ReactMarkdown from 'react-markdown'
 
+interface FileInfo { md: string; file_path: string }
+
 export function Root() {
 	const [content, setContent] = useState('')
 
 	useEffect(() => {
-		const unlisten = listen<{ md: string, file_path: string }>('open', (event) => {
+		const unlisten = listen<FileInfo>('open', (event) => {
 			setContent(event.payload.md)
 		})
 
