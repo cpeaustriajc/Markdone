f0bea424f99d516bd092ccc7d130702a677af71c
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2024-07-21 09:54:05 +0800

    Remove stack header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
+import './styles.css';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
 }
