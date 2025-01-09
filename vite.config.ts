3e298617b026da261860ae8bc819b59173079484
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2025-01-09 12:56:44 +0800

    Remove tauri dev host variable

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
-
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [reactRouter(), tsconfigPaths()],
 }));
