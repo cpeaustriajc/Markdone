37a1f917900029ba102efad1bae2a38dcd2963dd
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2025-01-11 13:21:10 +0800

    Add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import babel from "vite-plugin-babel";
 
 export default defineConfig(async () => ({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"],
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+    tsconfigPaths(),
+  ],
 }));
